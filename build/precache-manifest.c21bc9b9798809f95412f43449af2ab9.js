f04f18a052d6d2b9700f3a0776df8dad4f63def3
Ekaluk52003 <ekaluk.p@gmail.com>
2021-08-07 15:45:27 +0700

    first

---
+++ build/precache-manifest.c21bc9b9798809f95412f43449af2ab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef33a2960e3f171c0af71bac6661d0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a41b46f391b7e79295",
+    "url": "/static/css/main.e2bfd105.chunk.css"
+  },
+  {
+    "revision": "6ee62de87fd17c4b2c74",
+    "url": "/static/js/2.997d997c.chunk.js"
+  },
+  {
+    "revision": "94a41b46f391b7e79295",
+    "url": "/static/js/main.1108aec5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
